51df088a2fe2475802c8010a6318a896c8074ea5
Zhang Jing <jingz.bit@gmail.com>
2022-08-29 23:13:28 +0800

    Init upload

---
+++ app/src/main/java/com/dmtech/iw/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dmtech.iw;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_drawer_layout);
+    }
+}
